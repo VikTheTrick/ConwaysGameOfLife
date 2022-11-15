2394c18cea73d005a2bd6692a80fbdac7d3732d9
VikTheTrick <viktorvujicic1@gmail.com>
2022-11-15 01:13:19 +0100

    queue

--- procesi.py
+++ procesi.py
@@ -7,93 +7,120 @@ from multiprocessing import Process, Queue
 from time import sleep
 import pickle
 
-svetovi = []
-MAX_ITERACIJA = 5
-n = 5
-m = 5
+MAX_ITERACIJA = 10
+n = 10
+m = 10
 simulacija = []
 celije = []
+krajnjiSvetovi =[]
+
+class Poruka():
+    def __init__(self, stanje, x, y, iteracija):
+        self.stanje = stanje
+        self.x = x
+        self.y = y
+        self.iteracija = iteracija
 
 
 class Celija(Process):
-    def __init__(self, x, y, ziv):
+    def __init__(self, x, y, ziv, consumerQueue, iterationQueue):
         multiprocessing.Process.__init__(self)
         self.x = x
         self.y = y
         self.iteracija = 0
         self.procitaliMe = 0
         self.ziv = ziv
-        self.susedi = set()
-        self.redovi = []
+        self.susedi = dict()
         self.ziviSusedi = 0
-        self.pozicije = []
-        self.lock = multiprocessing.Lock()
-    def addQueue(self, queue1):
-        self.redovi.append(queue1)
+        self.consumerQueue = consumerQueue
+        self.iterationQueue = iterationQueue
+
+    def pozicija(self, x, y):
+        return (self.y * m + self.x) > (y * m + x)
 
     def iterate(self):
-        for index in range(len(self.redovi)):
-            if self.redovi[index].empty():
-                self.redovi[index].put(self.ziv)
+        for sused in self.susedi:
+            if self.pozicija(sused[0], sused[1]):
+                self.susedi[sused].put(self.ziv)
             else:
-                self.pozicije[index] = 1
-                self.ziviSusedi += self.redovi[index].get()
-                self.redovi[index].put(self.ziv)
-                self.redovi[index].put(self.ziv)
-
-        for index in range(len(self.redovi)):
-            if self.pozicije[index] == 0:
-                while self.redovi[index].qsize() != 2:
+                while self.susedi[sused].empty():
                     sleep(0.01)
-                self.ziviSusedi += self.redovi[index].get()
-                self.redovi[index].get()
-                self.procitaliMe += 1
-
-            else:
-                while not self.redovi[index].empty():
+                self.ziviSusedi += self.susedi[sused].get()
+        for sused in self.susedi:
+            if self.pozicija(sused[0], sused[1]):
+                while not self.susedi[sused].qsize() == 2:
                     sleep(0.01)
                 self.procitaliMe += 1
-
-        print(self.ziviSusedi)
+                self.ziviSusedi += self.susedi[sused].get()
+            else:
+                self.susedi[sused].put(self.ziv)
+                self.susedi[sused].put(1)
+                while not self.susedi[sused].qsize() == 1:
+                    sleep(0.01)
+                self.procitaliMe += self.susedi[sused].get()
         if self.ziv:
-            self.ziv = 1 if (self.ziviSusedi == 2 or self.ziviSusedi == 3) else 0
+            self.ziv = 1 if self.ziviSusedi == 2 or self.ziviSusedi == 3 else 0
         else:
-            self.ziv = 1 if (self.ziviSusedi == 3) else 0
-        svetovi[self.iteracija + 1][self.y, self.x] = self.ziv
+            self.ziv = 1 if self.ziviSusedi == 3 else 0
+
+        self.procitaliMe = 0
+        self.ziviSusedi = 0
+        self.iteracija += 1
+        self.consumerQueue.put(Poruka(self.ziv, self.x, self.y, self.iteracija))
+
+
+        while self.iterationQueue.qsize() != self.iteracija:
+            sleep(0.01)
 
     def run(self):
-        self.pozicije = [0] * (len(self.redovi))
         while self.iteracija < MAX_ITERACIJA:
             self.iterate()
-            self.ziviSusedi = 0
-            self.procitaliMe = 0
-            self.iteracija += 1
+            # cekamo da q size bude n*m
         return
 
 
-def loop(svet1):
-    svetovi.append(svet1)
-    for iterator in range(1, MAX_ITERACIJA + 1):
-        svetovi.append(numpy.zeros((n, m), int))
+def consumer(queue, svetovi, iterationQueue):
+    iteracija = 0
+    # consume work
+    global krajnjiSvetovi
+    while iteracija < MAX_ITERACIJA * n * m:
+        try:
+            item = queue.get()
+            iteracija += 1
+            if iteracija % (n * m) == 0:
+                print("desilo se")
+                iterationQueue.put(1)
+            svetovi[item.iteracija][item.y, item.x] = item.stanje
+        except queue.Empty:
+            sleep(0.01)
+    print("svetovi")
+    krajnjiSvetovi = svetovi
+    print(svetovi)
 
-    for iterator in range(0, n):
+
+def loop(svetovi):
+    consumerQueue = Queue()
+    iterationQueue = Queue()
+    consumerProcess = multiprocessing.Process(target=consumer, kwargs={'queue': consumerQueue, 'svetovi': svetovi, 'iterationQueue' : iterationQueue})
+    for i in range(0, n):
         for j in range(0, m):
-            celije.append(Celija(j, iterator, svet1[iterator, j]))
+            celije.append(Celija(j, i, svetovi[0][i, j], consumerQueue, iterationQueue))
 
     for c in celije:
-        for iterator in range(c.y - 1, c.y + 2):
+        for i in range(c.y - 1, c.y + 2):
             for j in range(c.x - 1, c.x + 2):
-                if iterator < 0 or iterator >= n or j < 0 or j >= m or (c.y == iterator and c.x == j): continue
+                if i < 0 or i >= n or j < 0 or j >= m or (c.y == i and c.x == j):
+                    continue
                 trenQueue = Queue()
-                imaGa = (j, iterator) in c.susedi
+                imaGa = (j, i) in c.susedi
                 if not imaGa:
-                    c.susedi.add((j, iterator))
-                    celije[(iterator * m + j)].susedi.add((c.x, c.y))
-                    c.addQueue(trenQueue)
-                    celije[(iterator * m + j)].addQueue(trenQueue)
+                    c.susedi[(j, i)] = trenQueue
+                    celije[i * m + j].susedi[(c.x, c.y)] = trenQueue
 
+    consumerProcess.start()
     for c in celije:
         c.start()
+    consumerProcess.join()
     for c in celije:
         c.join()
 
@@ -105,8 +132,12 @@ if __name__ == '__main__':
     svet[1, 1] = 1
     svet[1, 2] = 1
     svet[2, 1] = 1
-    simulacija.append(svet)
-    loop(svet)
+    svetovi = []
+    svetovi.append(svet)
+    for iterator in range(1, MAX_ITERACIJA + 1):
+        svetovi.append(numpy.zeros((n, m), int))
+    simulacija.append(svetovi)
+    loop(svetovi)
     print("kraj")
-    for i in svetovi:
+    for i in krajnjiSvetovi:
         print(i)
